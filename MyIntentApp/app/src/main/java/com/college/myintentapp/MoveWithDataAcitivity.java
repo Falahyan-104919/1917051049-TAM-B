f09bbad6e8e00045582e95c9a960f764d0f3efb2
falahyan-104919 <falahyan.104919@students.unila.ac.id>
2021-09-15 15:11:55 +0700

    commit coba

---
+++ MyIntentApp/app/src/main/java/com/college/myintentapp/MoveWithDataAcitivity.java
@@ -0,0 +1,24 @@
+package com.college.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MoveWithDataAcitivity extends AppCompatActivity {
+
+    public static String EXTRA_AGE = "extra_age";
+    public static  String EXTRA_NAME = "extra_name";
+    private TextView tvDataRecieved;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+        tvDataRecieved = (TextView) findViewById(R.id.tv_data_recieved);
+        String name = getIntent().getStringExtra(EXTRA_NAME);
+        int age = getIntent().getIntExtra(EXTRA_AGE, 0);
+        String text = "Name : "+name+", Your Age : "+age;
+        tvDataRecieved.setText(text);
+    }
+}
